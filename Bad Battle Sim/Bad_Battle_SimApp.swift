b997a8c789a2858f48116a52e5f93925693f3dd5
Eiron3636 <82043359+levivicars@users.noreply.github.com>
2021-09-22 10:22:13 -0500

    Initial Commit

---
+++ Bad Battle Sim/Bad_Battle_SimApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bad_Battle_SimApp.swift
+//  Bad Battle Sim
+//
+//  Created by Levi Vicars on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Bad_Battle_SimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bad Battle Sim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bad Battle Sim
+//
+//  Created by Levi Vicars on 9/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
