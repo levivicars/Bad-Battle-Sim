010153024e9923197399d2b592ffbfce899d52a7
Eiron3636 <82043359+levivicars@users.noreply.github.com>
2021-09-22 15:48:45 -0500

    - small greeting change .remove the disclaimer

---
+++ Bad Battle Sim/ContentModels/Monsters.swift
@@ -0,0 +1,20 @@
+//
+//  Monsters.swift
+//  Bad Battle Sim
+//
+//  Created by Levi Vicars on 9/22/21.
+//
+
+import Foundation
+
+
+class Monsters: Identifiable, Decodable {
+    
+    var name: String
+    var image: String
+    var id: Int
+    var AC: Int
+    var maxhealth: Int
+    var attackDamage: Int
+    
+}
