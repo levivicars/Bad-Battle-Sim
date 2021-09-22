df0d9a38b5710e95b26874f177813b180be96706
Eiron3636 <82043359+levivicars@users.noreply.github.com>
2021-09-22 11:56:09 -0500

    - add all image assets - build the StartView

---
+++ Bad Battle Sim/Views/RectangleCard.swift
@@ -0,0 +1,26 @@
+//
+//  RectangleCard.swift
+//  Bad Battle Sim
+//
+//  Created by Levi Vicars on 9/22/21.
+//
+
+import SwiftUI
+
+struct RectangleCard: View {
+    var color: Color
+    var body: some View {
+        Rectangle()
+            .cornerRadius(5)
+            .foregroundColor(color)
+            .frame(width: 375, height: 65)
+            .shadow(radius: 20)
+            
+    }
+}
+
+struct RectangleCard_Previews: PreviewProvider {
+    static var previews: some View {
+        RectangleCard(color: .white)
+    }
+}
--- Bad Battle Sim/Views/StartView.swift
+++ Bad Battle Sim/Views/StartView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            
+            Image("d&d")
+                .cornerRadius(10)
+                .scaleEffect(CGSize(width: 0.8, height: 0.8))
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                
+            
+            Spacer()
+            
+            Text("Hello, young adventurer. Welcome to the Bad Battle Simulation app, where you will face off with deadly monsters such as: The Bugbear, The Zombie, and much more!!!")
+                .font(.title)
+                .bold()
+                .padding()
+                .shadow(radius: 5)
+            Spacer()
+            
+            Button(action: {
+                
+                
+                
+            }, label: {
+                ZStack {
+                    RectangleCard(color: .green)
+                    Text("Begin Battle")
+                        .bold()
+                        .foregroundColor(.black)
+                }
+            })
+            
+            Spacer()
+                
+        }
     }
 }
 
